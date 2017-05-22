6a4785a76d22b3909268a631623b22897acd8ac0
azoladyantyi <aizydyantyi@gmail.com>
2017-05-22 17:40:16 +0200

    commit the changes i added on my files

--- Everyday_Decisions/my_screen.js
+++ Everyday_Decisions/my_screen.js
@@ -1,77 +1,98 @@
 // console.log(document.querySelector('#tableTemplate'));
 var tableTemplate = document.querySelector('#tableTemplate').innerHTML;
-if (tableTemplate) {
-    var tableTemplateInst = Handlebars.compile(tableTemplate);
-
-    var JobData = [{
-            id: "1",
-            jobTitle: 'back end developer',
-            JobType: 'Permanent',
-            jobLocation: 'waterfront',
-            NameOfCompany: 'Codex',
-            Age: '18 - 35 years',
-            Appy: 'Apply',
-            applications: []
-        },
-        {
-            id: "2",
-            jobTitle: 'Associate Developer',
-            JobType: '1 year contract',
-            jobLocation: 'Sea Point ',
-            NameOfCompany: 'Nedbank',
-            Age: '18 - 35 years',
-            Appy: 'Apply'
-        },
-
-        {
-            id: "3",
-            jobTitle: 'Application Developer',
-            JobType: 'Permanent',
-            jobLocation: 'Cape Town',
-            NameOfCompany: 'Media24',
-            Age: '18 - 35 years',
-            Appy: 'Apply'
-        }, {
-            id: "3",
-            jobTitle: 'Associate Developer',
-            JobType: '1 year contract',
-            jobLocation: 'Sea Point ',
-            NameOfCompany: 'Nedbank',
-            Age: '18 - 35 years',
-            Appy: 'Apply'
-        }, {
-            id: "4",
-            jobTitle: 'Application Support Analyster',
-            JobType: 'Permanent',
-            jobLocation: 'Cape Town ',
-            NameOfCompany: 'Amazon',
-            Age: '18 - 35 years',
-            Appy: 'Apply'
-        },
-    ];
+var tableTemplateInst = Handlebars.compile(tableTemplate);
 
 
-    var tableTemplateInst = Handlebars.compile(tableTemplate);
 
-    var displayTableResults = tableTemplateInst({
-        job: JobData
-    });
-    var tableDisplay = document.querySelector('#tableDisplay');
-    tableDisplay.innerHTML = displayTableResults;
-}
-var formData = [{
-    jobTitle: '1',
-    submit: 'submit'
-}, ];
+var JobData = [{
+        id: "1",
+        jobTitle: 'back end developer',
+        JobType: 'Permanent',
+        jobLocation: 'waterfront',
+        NameOfCompany: 'Codex',
+        Age: '18 - 35 years',
+        Appy: 'Apply',
+        ViewApplicant: 'ViewApplicant'
+    },
+    {
+        id: "2",
+        jobTitle: 'Associate Developer',
+        JobType: '1 year contract',
+        jobLocation: 'Sea Point ',
+        NameOfCompany: 'Nedbank',
+        Age: '18 - 35 years',
+        Appy: 'Apply',
+        ViewApplicant: 'ViewApplicant'
+    },
 
-var textBox = document.querySelector('#input');
+    {
+        id: "3",
+        jobTitle: 'Application Developer',
+        JobType: 'Permanent',
+        jobLocation: 'Cape Town',
+        NameOfCompany: 'Media24',
+        Age: '18 - 35 years',
+        Appy: 'Apply',
+        ViewApplicant: 'ViewApplicant'
+    }, {
+        id: "3",
+        jobTitle: 'Associate Developer',
+        JobType: '1 year contract',
+        jobLocation: 'Sea Point ',
+        NameOfCompany: 'Nedbank',
+        Age: '18 - 35 years',
+        Appy: 'Apply',
+        ViewApplicant: 'ViewApplicant'
+    }, {
+        id: "4",
+        jobTitle: 'Application Support Analyster',
+        JobType: 'Permanent',
+        jobLocation: 'Cape Town ',
+        NameOfCompany: 'Amazon',
+        Age: '18 - 35 years',
+        Appy: 'Apply',
+        ViewApplicant: 'ViewApplicant'
+    },
+];
 
+function showJobs(){
+  var displayTableResults = tableTemplateInst({
+      job: JobData
+  });
+
+  var tableDisplay = document.querySelector('#tableDisplay');
+  tableDisplay.innerHTML = displayTableResults;
+
+
+  //aapplicationForm.innerHTML = ""
+  //applicant.innerHTML = ""
+  //backToApp.innerHTML = ""
+
+
+};
+
+showJobs();
+
+var viewApplicantBtn = document.querySelector("#viewApplicant");
 function submitFunction(jobTitle) {
+  var personalDetails = [];
+  for (var i = 0; i < JobData.length; i++) {
+
+  if ( viewApplicantBtn.dataset.jobsJobTitle===JobData[i].jobTitle ) {
+      personalDetails.push({
+        name
+      })
+
+
+  }
+  }
+
     var tableTemplateInst = Handlebars.compile(tableTemplate);
 
     var displayTableResults = tableTemplateInst({
         job: JobData
     });
+
     tableDisplay.innerHTML = displayTableResults;
     applicationForm.innerHTML = ""
     applicant.innerHTML = ""
@@ -81,15 +102,14 @@ function submitFunction(jobTitle) {
 
 //set up my empty div
 var applicationForm = document.querySelector('#applicationForm');
-//
+
 // //setup the application template
 var applicationTemplate = document.querySelector("#applicationTemplate").innerHTML;
-// //compile the applicationForm
-var applicationInst = Handlebars.compile(applicationTemplate);
-var displayForm = applicationInst({})
-//show results
-applicationForm.innerHTML = ""
 
+// //compile the applicationForm
+const applicationInst = Handlebars.compile(applicationTemplate);
+//show results
+//applicationForm.innerHTML = ""
 
 function formFunction(jobId) {
     var itJob = JobData.find(function(f) {
@@ -107,59 +127,92 @@ function formFunction(jobId) {
 
     tableDisplay.innerHTML = "This job has been viewed " + itJob.viewCounter + " time(s)."
 
+    var displayForm = applicationInst(itJob)
 
     applicationForm.innerHTML = displayForm
 }
 
 
 var applicant = document.querySelector("#applicant");
+// applicant.innerHTML = applicant
 var candidateTemplate = document.querySelector("#candidateTemplate").innerHTML;
 var candidateInst = Handlebars.compile(candidateTemplate);
 
 var FullDetail = [{
+  // jobTitle: 1
     name: "azola",
     surname: "Dyantyi",
     Age: 25,
     email: "aizydyantyi@gmail.com",
-    applications: []
+
 }, ];
 var candiatateDetails = candidateInst({
     title: FullDetail
 
 })
 
-var saveBtn = document.querySelector('#saveBtn');
 
-saveBtn.addEventListener('click', function() {
-  var name = document.getElementById("name");
-  var surname = document.getElementById("surname");
-  var Age = document.getElementById("Age");
-  var email = document.getElementById("email");
-if (name !== undefined && surname !== undefined && Age!== undefined && email !== undefined) {
-    FullDetail.push({
-        name: name.value,
-        surname: surname.value,
-        Age: Number(Age.value),
-        email: email.value
+
+var candidateInst = Handlebars.compile(candidateTemplate);
+
+function canditateFunction(jobId) {
+
+    var itJob = JobData.find(function(f) {
+      return f.id === jobId;
+    });
+
+    var candiatateDetails = candidateInst({
+        title: FullDetail
     })
+    var name = document.getElementById("name");
+    var surname = document.getElementById("surname");
+    var Age = document.getElementById("age");
+    var email = document.getElementById("email");
+
+    if (name !== undefined && surname !== undefined && Age !== undefined && email !== undefined) {
+
+        if (!itJob.applications){
+          itJob.applications = [];
+        }
+
+        itJob.applications.push({
+            name: name.value,
+            surname: surname.value,
+            Age: Number(Age.value),
+            email: email.value
+        });
+
+        showJobs();
+
+
+    }
+
+    //applicant(FullDetail)
 }
-applicant(FullDetail)
-});
 
-function applicantFunction() {
-  var candidateInst = Handlebars.compile(candidateTemplate);;
+var candidateInst = Handlebars.compile(candidateTemplate);
 
-  var candiatateDetails = candidateInst({
-      title: FullDetail
-  })
+function applicantFunction(jobId) {
+
+    var itJob = JobData.find(function(f) {
+      return f.id === jobId;
+    });
+
+    var candiatateDetails = candidateInst({
+        title: itJob.applications
+    })
     applicant.innerHTML = candiatateDetails
     tableDisplay.innerHTML = ""
     applicationForm.innerHTML = ""
 }
+
 var backToApp = document.querySelector("#backToApp");
+
 function backToAppFunction() {
-  applicationForm.innerHTML =   displayForm
-  tableDisplay.innerHTML = ""
-  applicant.innerHTML = ""
+    // applicationForm.innerHTML = displayForm
+    // tableDisplay.innerHTML = ""
+    // applicant.innerHTML = ""
+    showJobs();
+
 }
 backToApp.innerHTML = ""
